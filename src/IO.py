74503ba69397c3963d099a49ca81d54fd42b154e
linuseing <linus@r13.de>
2021-05-19 10:40:46 +0200

    filter services

--- src/IO.py
+++ src/IO.py
@@ -64,10 +64,23 @@ class IO:
 
         self.load_formatter()
 
-        core.api.gql.add_query("availableOutputServices: [OutputService]!", self.handle_available_output_services)
+        core.api.gql.add_query(
+            "availableOutputServices(domain: String, contains: String): [OutputService]!",
+            self.handle_available_output_services
+        )
         core.api.gql.add_query("availableInputServices: [String]!", self.handle_available_input_services)
 
-    async def handle_available_output_services(self, *_):
+    async def handle_available_output_services(self, *_, domain=None, contains=None):
+        if domain:
+            return list(filter(
+                lambda x: x['name'].split('.')[0] == domain,
+                map(lambda s: s.gql, self._output_services.values())
+            ))
+        if contains:
+            return list(filter(
+                lambda x: contains in x['name'],
+                map(lambda s: s.gql, self._output_services.values())
+            ))
         return list(map(lambda s: s.gql, self._output_services.values()))
 
     async def handle_available_input_services(self, *_):
