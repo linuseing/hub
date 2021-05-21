94d20a12d433278822dee67e4192e348a0267752
linuseing <linus@r13.de>
2021-05-21 11:49:15 +0200

    generator style recursive import

--- src/helper/package_loader.py
+++ src/helper/package_loader.py
@@ -1,4 +1,5 @@
 import importlib
+import os
 import pkgutil
 
 
@@ -18,3 +19,20 @@ def import_submodules(package, recursive=False, blacklist=None) -> dict:
         if recursive and is_pkg:
             results.update(import_submodules(full_name))
     return results
+
+
+def submodules(package, recursive=False, blacklist=None):
+
+    if blacklist is None:
+        blacklist = []
+
+    if isinstance(package, str):
+        package = importlib.import_module(package)
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
+        full_name = package.__name__ + "." + name
+        if full_name in blacklist:
+            continue
+        module = importlib.import_module(full_name)
+        yield name, os.path.dirname(module.__file__), module
+        if recursive and is_pkg:
+            yield from submodules(full_name)
